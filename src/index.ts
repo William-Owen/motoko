bf5d78b0278905171386c385569915d5597e7cf2
William Owen <git@wo.dev>
2025-08-05 14:56:59 +0100

    next (#12)
    * Updated release notes generation process
    
    * fixed exports

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 // src/index.ts
 
-export { default as NameValue } from "./components/NameValue/"
-export { default as Page } from "./components/Page/"
-export { default as PageTitle } from "./components/PageTitle/"
-export { default as VisuallyHidden } from "./components/VisuallyHidden"
+export { default as NameValue } from "./components/NameValue/index"
+export { default as Page } from "./components/Page/index"
+export { default as PageTitle } from "./components/PageTitle/index"
+export { default as VisuallyHidden } from "./components/VisuallyHidden/index"
 
 export { cn, cssBoxSpacing, formatDate, formatNumber } from "./utils"
